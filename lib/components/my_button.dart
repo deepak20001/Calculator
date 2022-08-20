0eedcb390e9cab24499236bee690e4a75b2a747f
Deepak <djamwal10@gmail.com>
2022-08-20 12:47:54 +0530

    used 2 expanded widgets to have space for output and given flex = 2 for 2nd expanded widget so that it can occupy double space then 1st one

--- lib/components/my_button.dart
+++ lib/components/my_button.dart
@@ -13,14 +13,17 @@ class MyButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Expanded(
-      child: InkWell(
-        child: Container(
-          height: 80,
-          decoration: BoxDecoration(shape: BoxShape.circle, color: colors),
-          child: Center(
-            child: Text(
-              title,
-              style: TextStyle(fontSize: 20, color: Colors.white),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 3),
+        child: InkWell(
+          child: Container(
+            height: 80,
+            decoration: BoxDecoration(shape: BoxShape.circle, color: colors),
+            child: Center(
+              child: Text(
+                title,
+                style: TextStyle(fontSize: 20, color: Colors.white),
+              ),
             ),
           ),
         ),
--- lib/home_page.dart
+++ lib/home_page.dart
@@ -10,100 +10,130 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  var userInput = '';
+  var answer = '';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black,
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 30),
           child: Column(
             children: [
-              Row(
-                children: [
-                  MyButton(
-                    title: "AC",
+//-----------------------------------------------------------------
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 20),
+                  child: Column(
+                    children: [
+                      Text(
+                        userInput.toString(),
+                        style: TextStyle(fontSize: 30, color: Colors.white),
+                      ),
+                      Text(
+                        userInput.toString(),
+                        style: TextStyle(fontSize: 30, color: Colors.white),
+                      ),
+                    ],
                   ),
-                  MyButton(
-                    title: "+/-",
-                  ),
-                  MyButton(
-                    title: "%",
-                  ),
-                  MyButton(
-                    title: "/",
-                    colors: Colors.grey,
-                  ),
-                ],
+                ),
               ),
-              Row(
-                children: [
-                  MyButton(
-                    title: "7",
-                  ),
-                  MyButton(
-                    title: "8",
-                  ),
-                  MyButton(
-                    title: "0",
-                  ),
-                  MyButton(
-                    title: "x",
-                    colors: Colors.grey,
-                  ),
-                ],
+//--------------------------------------------------------------------
+              Expanded(
+                flex: 2,
+                child: Column(
+                  children: [
+                    Row(
+                      children: [
+                        MyButton(
+                          title: "AC",
+                        ),
+                        MyButton(
+                          title: "+/-",
+                        ),
+                        MyButton(
+                          title: "%",
+                        ),
+                        MyButton(
+                          title: "/",
+                          colors: Colors.grey,
+                        ),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        MyButton(
+                          title: "7",
+                        ),
+                        MyButton(
+                          title: "8",
+                        ),
+                        MyButton(
+                          title: "0",
+                        ),
+                        MyButton(
+                          title: "x",
+                          colors: Colors.grey,
+                        ),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        MyButton(
+                          title: "4",
+                        ),
+                        MyButton(
+                          title: "5",
+                        ),
+                        MyButton(
+                          title: "6",
+                        ),
+                        MyButton(
+                          title: "-",
+                          colors: Colors.grey,
+                        ),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        MyButton(
+                          title: "1",
+                        ),
+                        MyButton(
+                          title: "2",
+                        ),
+                        MyButton(
+                          title: "3",
+                        ),
+                        MyButton(
+                          title: "+",
+                          colors: Colors.grey,
+                        ),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        MyButton(
+                          title: "0",
+                        ),
+                        MyButton(
+                          title: ".",
+                        ),
+                        MyButton(
+                          title: "DEL",
+                        ),
+                        MyButton(
+                          title: "=",
+                          colors: Colors.grey,
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 10),
+                  ],
+                ),
               ),
-              Row(
-                children: [
-                  MyButton(
-                    title: "4",
-                  ),
-                  MyButton(
-                    title: "5",
-                  ),
-                  MyButton(
-                    title: "6",
-                  ),
-                  MyButton(
-                    title: "-",
-                    colors: Colors.grey,
-                  ),
-                ],
-              ),
-              Row(
-                children: [
-                  MyButton(
-                    title: "1",
-                  ),
-                  MyButton(
-                    title: "2",
-                  ),
-                  MyButton(
-                    title: "3",
-                  ),
-                  MyButton(
-                    title: "+",
-                    colors: Colors.grey,
-                  ),
-                ],
-              ),
-              Row(
-                children: [
-                  MyButton(
-                    title: "0",
-                  ),
-                  MyButton(
-                    title: ".",
-                  ),
-                  MyButton(
-                    title: "DEL",
-                  ),
-                  MyButton(
-                    title: "=",
-                    colors: Colors.grey,
-                  ),
-                ],
-              ),
-              SizedBox(height: 10),
             ],
           ),
         ),
